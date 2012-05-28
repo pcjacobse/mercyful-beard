ad40968d382aa93141cd22f8465cd95bf1add950
Paul Jacobse <pcjacobse@gmail.com>
2012-05-28 09:28:03 +0200

    Changed American Dad! episode map. Preventing infinite loop

--- data/73141.php
+++ data/73141.php
@@ -2,8 +2,8 @@
 # American Dad!!!!
 
 function convert($v){
-  $tvdb = array(0,7,16,19,16,20,18,19,16);
-  $scene = array(0,23,19,16,20,18,19,16);
+  $tvdb = array(2,7,16,19,16,20,18,19,18);
+  $scene = array(2,23,19,16,20,18,19,18);
   return episode(absolute($v,$tvdb),$scene);
 }
 ?>
